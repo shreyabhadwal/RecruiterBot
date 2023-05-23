6bc26db3fa4e296c1a9fa28268c9168eeb9c2f57
Tirthesh271 <tirtheshsohani@gmail.com>
2023-05-23 16:18:56 +0530

    Responsive UI implemented

--- ChatBot/static/js/jquery.js
+++ ChatBot/static/js/jquery.js
@@ -1,8 +1,12 @@
 
 function getBotResponse() {
     var rawText = $("#textInput").val();
-    var userHtml = '<div class = "textBoxUser"><img  class= "logo" src="../static/images/userLogo.png") }}" alt="userLogo" /><p class="userText">  <span>' + rawText + "</span></p></div>";
+    var text = rawText.replace(/\r|\n/g, "<br>");
+    // console.log(text);
+    // console.log(rawText);
+    var userHtml = '<div class = "textBoxUser"><img  class= "logo" src="../static/images/userLogo.png") }}" alt="userLogo" /><p class="userText">  <span>' + text + "</span></p></div>";
     // var userHtml = '<div class = "textBoxUser"><img  class= "logo" src="../static/images/userLogo.png" alt="userLogo" /><p class="userText">  <span>' + rawText + "</span></p></div>";
+
     $("#textInput").val("");
     $("#chatbox").append(userHtml);
     document
@@ -16,11 +20,27 @@ function getBotResponse() {
             .getElementById("userInput")
             .scrollIntoView({ block: "start", behavior: "smooth" });
     });
+    // Reset text box size
+    textarea = document.querySelector("#textInput");
+    this.style.height = '1.5em';
 }
 
 //Get output on pressing enter
 $("#textInput").keypress(function (e) {
+
     if (e.which == 13) {
-        getBotResponse();
+        if (e.shiftKey) {
+            e.preventDefault()
+            getBotResponse();
+        }
     }
 });
+
+textarea = document.querySelector("#textInput");
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
+
--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,14 @@
 FROM python:3.9-slim-buster
 
-WORKDIR /ChatBot
+WORKDIR /app
 
-COPY requirements.txt .
+COPY requirements.txt ./
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 
 COPY ChatBot/ .
 COPY ChatBot/faiss_index /app/faiss_index
 
 EXPOSE 5000
-
-ENTRYPOINT ["python"]
+ENTRYPOINT ["python3"]
 CMD ["index.py"]
